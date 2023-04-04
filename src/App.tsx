413a22af03ea9e13a418efae8a916552d1513565
Tayan21 <bostybaev@gmail.com>
2023-04-04 12:54:01 +0600

    add changes to path

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const Cart = loadable({
 const App: React.FC = () => {
   return (
     <Routes>
-      <Route path="react-pizza/" element={<MainLayout />}>
+      <Route path="/" element={<MainLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="cart" element={<Cart />} />
         <Route path="pizza/:id" element={<FullPizza />} />
